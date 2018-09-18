dd341fcd5c4ab6bbf033790361afb20c0bb5b91f
Jred <freddy.demo@gmail.com>
2018-09-18 12:26:18 -0400

    faltaba el de titulos

---
+++ SQL INSERTION/titulos.sql
@@ -0,0 +1,5 @@
+INSERT INTO `titulos` VALUES (1, 'Lic.', NULL, NULL);
+INSERT INTO `titulos` VALUES (2, 'Msc.', NULL, NULL);
+INSERT INTO `titulos` VALUES (3, 'Msc. Lic.', NULL, NULL);
+INSERT INTO `titulos` VALUES (4, 'Msc. Ing.', NULL, NULL);
+INSERT INTO `titulos` VALUES (5, 'Ing.', NULL, NULL);
