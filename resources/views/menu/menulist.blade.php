33e8ecba3eaac143080369ab779754c5acdcfb7e
Jred <freddy.demo@gmail.com>
2018-10-28 19:34:12 -0400

    gestion CRUD

--- resources/views/menu/menulist.blade.php
+++ resources/views/menu/menulist.blade.php
@@ -77,6 +77,14 @@
                             </g>
                           </svg>
                         </a></li>
+                        <li><a href="{{ route('gestion.create') }}">Registrar gestion
+                          <svg version="1.1" id="Layer_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                             width="9px" height="5px" viewBox="0 0 9 5" enable-background="new 0 0 9 5" xml:space="preserve">
+                            <g>
+                              <polyline class="arrow_d" fill="none" stroke="#FFFFFF" stroke-miterlimit="10" points="0.022,-0.178 4.5,4.331 9.091,-0.275   "/>
+                            </g>
+                          </svg>
+                        </a></li>
                       </ul>
                     </li>
                     <li class="active hassubs">
@@ -130,6 +138,14 @@
                           </svg>
                         </a></li>
                         <li><a href="{{ route('modalidad.index') }}">Lista de Modalidades
+                          <svg version="1.1" id="Layer_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                             width="9px" height="5px" viewBox="0 0 9 5" enable-background="new 0 0 9 5" xml:space="preserve">
+                            <g>
+                              <polyline class="arrow_d" fill="none" stroke="#FFFFFF" stroke-miterlimit="10" points="0.022,-0.178 4.5,4.331 9.091,-0.275   "/>
+                            </g>
+                          </svg>
+                        </a></li>
+                        <li><a href="{{ route('gestion.index') }}">Lista de Gestiones
                           <svg version="1.1" id="Layer_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
                              width="9px" height="5px" viewBox="0 0 9 5" enable-background="new 0 0 9 5" xml:space="preserve">
                             <g>
